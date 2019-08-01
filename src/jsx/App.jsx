3a45e535039d92a33b2450d62ebeb6f683cbbd7d
cadenzah <cadenzah93@gmail.com>
2019-08-01 13:41:37 +0900

    search and data fetch features - used 'axios' module - 'Input' component

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,10 +1,54 @@
 const React = require('react')
+const axios = require('axios')
+let youtubeAPIURL = `https://www.googleapis.com/youtube/v3/search`
+  + `?part=snippet`
+  + `&maxResults=5`
+  + `&order=relevance`
+  + `&key=AIzaSyD3sCnZg05jineQg2Puzee12EYXX4E9XA4`
+
+//   + `&q=zerg`
+
+const Input = require('./Input.jsx')
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      keyword: "",
+      data: {}
+    }
+  }
+
+  handleSearchInput(e) {
+    this.setState({ keyword: e.target.value })
+  }
+
+  handleFetchData(e) {
+    const newYoutubeAPIURL = youtubeAPIURL + `&q=${this.state.keyword}`
+    axios.get(newYoutubeAPIURL)
+      .then((response) => {
+        this.setState({ data: response })
+        return new Promise(resolve => {
+          resolve(response)
+        })
+      }).then((data) => {
+         console.log(this.state.data)
+      })
+  }
+
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <div>
-        Hello, React! ft. Babel and Webpack
+        <h2>Youtube Search & Player</h2>
+        <Input
+          onInputChange = {(e) => this.handleSearchInput(e)}
+          onSearchExecuted = {(e) => this.handleFetchData(e)}
+          searchKeyword = {this.state.keyword}/>
+        <h5>Youtube API Key is provided by cadenzah. DON't ABUSE!</h5>
       </div>
     )
   }
---
+++ src/jsx/Input.jsx
@@ -0,0 +1,19 @@
+const React = require('react')
+
+const Input = (props) => {
+  return (
+    <div className="search-bar">
+      <input
+        className = "search-bar-input"
+        type = "text"
+        placeholder = "검색 키워드를 입력하세요"
+        onChange = {props.onInputChange}
+        value = {props.searchKeyword}/>
+      <button
+        className = "search-bar-button"
+        onClick = {props.onSearchExecuted}>검색</button>
+    </div>
+  )
+}
+
+module.exports = Input
